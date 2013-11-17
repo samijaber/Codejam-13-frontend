1c8d794d0bb427f03c670fd241bac591c54e7d8d
tinalatif <tina.tl.latif@gmail.com>
2013-11-16 21:13:39 -0500

    fixed dates

--- linechart.js
+++ linechart.js
@@ -6,7 +6,7 @@ nv.addGraph(function() {
   chart = nv.models.lineChart()
   .options({
     margin: {left: 100, bottom: 100},
-    x: function(d,i) { return i},
+    //x: function(d,i) { return i},
     showXAxis: true,
     showYAxis: true,
     transitionDuration: 250
@@ -14,10 +14,10 @@ nv.addGraph(function() {
   ;
 
   // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
-  chart.xAxis
-       .axisLabel('Date')
-       .rotateLabels(-45)
-       .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)); });
+    chart.xAxis
+         .axisLabel('Date')
+         .rotateLabels(-45)
+         .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)); });
 
     //.tickFormat(d3.format(',.1f'));
      // .tickFormat(function(d) {
@@ -49,72 +49,71 @@ nv.addGraph(function() {
 
 function populate() {
   var arr = [];
-  arr.push({x: })
   
-  // arr.push({x: new Date(2013, 11, 09, 08, 00, 00, 00), y: 14369.872});
-  // arr.push({x: new Date(2013, 11, 09, 08, 15, 00, 00), y: 14425.662});
-  // arr.push({x: new Date(2013, 11, 09, 08, 30, 00, 00), y: 14467.903});
-  // arr.push({x: new Date(2013, 11, 09, 08, 45, 00, 00), y: 14563.201});
-  // arr.push({x: new Date(2013, 11, 09, 09, 00, 00, 00), y: 14780.018});
-  // arr.push({x: new Date(2013, 11, 09, 09, 15, 00, 00), y: 14758.391});
-  // arr.push({x: new Date(2013, 11, 09, 09, 30, 00, 00), y: 14759.164});
-  // arr.push({x: new Date(2013, 11, 09, 09, 45, 00, 00), y: 14866.025});
-  // arr.push({x: new Date(2013, 11, 09, 10, 00, 00, 00), y: 15173.152});
-  // arr.push({x: new Date(2013, 11, 09, 10, 15, 00, 00), y: 15218.152});
-  // arr.push({x: new Date(2013, 11, 09, 10, 30, 00, 00), y: 15248.415});
-  // arr.push({x: new Date(2013, 11, 09, 10, 45, 00, 00), y: 15310.935});
-  // arr.push({x: new Date(2013, 11, 09, 11, 00, 00, 00), y: 15529.061});
-  // arr.push({x: new Date(2013, 11, 09, 11, 15, 00, 00), y: 15641.979});
-  // arr.push({x: new Date(2013, 11, 09, 11, 30, 00, 00), y: 15598.05});
-  // arr.push({x: new Date(2013, 11, 09, 11, 45, 00, 00), y: 15753.887});
-  // arr.push({x: new Date(2013, 11, 09, 12, 00, 00, 00), y: 15777.439});
-  // arr.push({x: new Date(2013, 11, 09, 12, 15, 00, 00), y: 15801.726});
-  // arr.push({x: new Date(2013, 11, 09, 12, 30, 00, 00), y: 15856.039});
-  // arr.push({x: new Date(2013, 11, 09, 12, 45, 00, 00), y: 15815.965});
-  // arr.push({x: new Date(2013, 11, 09, 13, 00, 00, 00), y: 15824.856});
-  // arr.push({x: new Date(2013, 11, 09, 13, 15, 00, 00), y: 15764.931});
-  // arr.push({x: new Date(2013, 11, 09, 13, 30, 00, 00), y: 15743.37});
-  // arr.push({x: new Date(2013, 11, 09, 13, 45, 00, 00), y: 15800.409});
-  // arr.push({x: new Date(2013, 11, 09, 14, 00, 00, 00), y: 15788.381});
-  // arr.push({x: new Date(2013, 11, 09, 14, 15, 00, 00), y: 15678.741});
-  // arr.push({x: new Date(2013, 11, 09, 14, 30, 00, 00), y: 15677.23});
-  // arr.push({x: new Date(2013, 11, 09, 14, 45, 00, 00), y: 15817.903});
-  // arr.push({x: new Date(2013, 11, 09, 15, 00, 00, 00), y: 15888.88});
-  // arr.push({x: new Date(2013, 11, 09, 15, 15, 00, 00), y: 15845.501});
-  // arr.push({x: new Date(2013, 11, 09, 15, 30, 00, 00), y: 15926.713});
-  // arr.push({x: new Date(2013, 11, 09, 15, 45, 00, 00), y: 15922.235});
-  // arr.push({x: new Date(2013, 11, 09, 16, 00, 00, 00), y: 15684.123});
-  // arr.push({x: new Date(2013, 11, 09, 16, 15, 00, 00), y: 15657.75});
-  // arr.push({x: new Date(2013, 11, 09, 16, 30, 00, 00), y: 15808.328});
-  // arr.push({x: new Date(2013, 11, 09, 16, 45, 00, 00), y: 15922.424});
-  // arr.push({x: new Date(2013, 11, 09, 17, 00, 00, 00), y: 15771.909});
-  // arr.push({x: new Date(2013, 11, 09, 17, 15, 00, 00), y: 15758.857});
-  // arr.push({x: new Date(2013, 11, 09, 17, 30, 00, 00), y: 15634.568});
-  // arr.push({x: new Date(2013, 11, 09, 17, 45, 00, 00), y: 15613.649});
-  // arr.push({x: new Date(2013, 11, 09, 18, 00, 00, 00), y: 15208.584});
-  // arr.push({x: new Date(2013, 11, 09, 18, 15, 00, 00), y: 15273.769});
-  // arr.push({x: new Date(2013, 11, 09, 18, 30, 00, 00), y: 15267.812});
-  // arr.push({x: new Date(2013, 11, 09, 18, 45, 00, 00), y: 15215.534});
-  // arr.push({x: new Date(2013, 11, 09, 19, 00, 00, 00), y: 15214.557});
-  // arr.push({x: new Date(2013, 11, 09, 19, 15, 00, 00), y: 15198.033});
-  // arr.push({x: new Date(2013, 11, 09, 19, 30, 00, 00), y: 15159.63});
-  // arr.push({x: new Date(2013, 11, 09, 19, 45, 00, 00), y: 15125.667});
-  // arr.push({x: new Date(2013, 11, 09, 20, 00, 00, 00), y: 15028.004});
-  // arr.push({x: new Date(2013, 11, 09, 20, 15, 00, 00), y: 14970.92});
-  // arr.push({x: new Date(2013, 11, 09, 20, 30, 00, 00), y: 14919.566});
-  // arr.push({x: new Date(2013, 11, 09, 20, 45, 00, 00), y: 14923.107});
-  // arr.push({x: new Date(2013, 11, 09, 21, 00, 00, 00), y: 14782.877});
-  // arr.push({x: new Date(2013, 11, 09, 21, 15, 00, 00), y: 14719.747});
-  // arr.push({x: new Date(2013, 11, 09, 21, 30, 00, 00), y: 14709.362});
-  // arr.push({x: new Date(2013, 11, 09, 21, 45, 00, 00), y: 14747.927});
-  // arr.push({x: new Date(2013, 11, 09, 22, 00, 00, 00), y: 14573.228});
-  // arr.push({x: new Date(2013, 11, 09, 22, 15, 00, 00), y: 14442.517});
-  // arr.push({x: new Date(2013, 11, 09, 22, 30, 00, 00), y: 14424.157});
-  // arr.push({x: new Date(2013, 11, 09, 22, 45, 00, 00), y: 14389.285});
-  // arr.push({x: new Date(2013, 11, 09, 23, 00, 00, 00), y: 14408.623});
-  // arr.push({x: new Date(2013, 11, 09, 23, 15, 00, 00), y: 14458.237});
-  // arr.push({x: new Date(2013, 11, 09, 23, 30, 00, 00), y: 14401.343});
-  // arr.push({x: new Date(2013, 11, 09, 23, 45, 00, 00), y: 14390.64});
+  arr.push({x: new Date(2013, 11 -1, 09, 08, 00, 00, 00), y: 14369.872});
+  arr.push({x: new Date(2013, 11 -1, 09, 08, 15, 00, 00), y: 14425.662});
+  arr.push({x: new Date(2013, 11 -1, 09, 08, 30, 00, 00), y: 14467.903});
+  arr.push({x: new Date(2013, 11 -1, 09, 08, 45, 00, 00), y: 14563.201});
+  arr.push({x: new Date(2013, 11 -1, 09, 09, 00, 00, 00), y: 14780.018});
+  arr.push({x: new Date(2013, 11 -1, 09, 09, 15, 00, 00), y: 14758.391});
+  arr.push({x: new Date(2013, 11 -1, 09, 09, 30, 00, 00), y: 14759.164});
+  arr.push({x: new Date(2013, 11 -1, 09, 09, 45, 00, 00), y: 14866.025});
+  arr.push({x: new Date(2013, 11 -1, 09, 10, 00, 00, 00), y: 15173.152});
+  arr.push({x: new Date(2013, 11 -1, 09, 10, 15, 00, 00), y: 15218.152});
+  arr.push({x: new Date(2013, 11 -1, 09, 10, 30, 00, 00), y: 15248.415});
+  arr.push({x: new Date(2013, 11 -1, 09, 10, 45, 00, 00), y: 15310.935});
+  arr.push({x: new Date(2013, 11 -1, 09, 11, 00, 00, 00), y: 15529.061});
+  arr.push({x: new Date(2013, 11 -1, 09, 11, 15, 00, 00), y: 15641.979});
+  arr.push({x: new Date(2013, 11 -1, 09, 11, 30, 00, 00), y: 15598.05});
+  arr.push({x: new Date(2013, 11 -1, 09, 11, 45, 00, 00), y: 15753.887});
+  arr.push({x: new Date(2013, 11 -1, 09, 12, 00, 00, 00), y: 15777.439});
+  arr.push({x: new Date(2013, 11 -1, 09, 12, 15, 00, 00), y: 15801.726});
+  arr.push({x: new Date(2013, 11 -1, 09, 12, 30, 00, 00), y: 15856.039});
+  arr.push({x: new Date(2013, 11 -1, 09, 12, 45, 00, 00), y: 15815.965});
+  arr.push({x: new Date(2013, 11 -1, 09, 13, 00, 00, 00), y: 15824.856});
+  arr.push({x: new Date(2013, 11 -1, 09, 13, 15, 00, 00), y: 15764.931});
+  arr.push({x: new Date(2013, 11 -1, 09, 13, 30, 00, 00), y: 15743.37});
+  arr.push({x: new Date(2013, 11 -1, 09, 13, 45, 00, 00), y: 15800.409});
+  arr.push({x: new Date(2013, 11 -1, 09, 14, 00, 00, 00), y: 15788.381});
+  arr.push({x: new Date(2013, 11 -1, 09, 14, 15, 00, 00), y: 15678.741});
+  arr.push({x: new Date(2013, 11 -1, 09, 14, 30, 00, 00), y: 15677.23});
+  arr.push({x: new Date(2013, 11 -1, 09, 14, 45, 00, 00), y: 15817.903});
+  arr.push({x: new Date(2013, 11 -1, 09, 15, 00, 00, 00), y: 15888.88});
+  arr.push({x: new Date(2013, 11 -1, 09, 15, 15, 00, 00), y: 15845.501});
+  arr.push({x: new Date(2013, 11 -1, 09, 15, 30, 00, 00), y: 15926.713});
+  arr.push({x: new Date(2013, 11 -1, 09, 15, 45, 00, 00), y: 15922.235});
+  arr.push({x: new Date(2013, 11 -1, 09, 16, 00, 00, 00), y: 15684.123});
+  arr.push({x: new Date(2013, 11 -1, 09, 16, 15, 00, 00), y: 15657.75});
+  arr.push({x: new Date(2013, 11 -1, 09, 16, 30, 00, 00), y: 15808.328});
+  arr.push({x: new Date(2013, 11 -1, 09, 16, 45, 00, 00), y: 15922.424});
+  arr.push({x: new Date(2013, 11 -1, 09, 17, 00, 00, 00), y: 15771.909});
+  arr.push({x: new Date(2013, 11 -1, 09, 17, 15, 00, 00), y: 15758.857});
+  arr.push({x: new Date(2013, 11 -1, 09, 17, 30, 00, 00), y: 15634.568});
+  arr.push({x: new Date(2013, 11 -1, 09, 17, 45, 00, 00), y: 15613.649});
+  arr.push({x: new Date(2013, 11 -1, 09, 18, 00, 00, 00), y: 15208.584});
+  arr.push({x: new Date(2013, 11 -1, 09, 18, 15, 00, 00), y: 15273.769});
+  arr.push({x: new Date(2013, 11 -1, 09, 18, 30, 00, 00), y: 15267.812});
+  arr.push({x: new Date(2013, 11 -1, 09, 18, 45, 00, 00), y: 15215.534});
+  arr.push({x: new Date(2013, 11 -1, 09, 19, 00, 00, 00), y: 15214.557});
+  arr.push({x: new Date(2013, 11 -1, 09, 19, 15, 00, 00), y: 15198.033});
+  arr.push({x: new Date(2013, 11 -1, 09, 19, 30, 00, 00), y: 15159.63});
+  arr.push({x: new Date(2013, 11 -1, 09, 19, 45, 00, 00), y: 15125.667});
+  arr.push({x: new Date(2013, 11 -1, 09, 20, 00, 00, 00), y: 15028.004});
+  arr.push({x: new Date(2013, 11 -1, 09, 20, 15, 00, 00), y: 14970.92});
+  arr.push({x: new Date(2013, 11 -1, 09, 20, 30, 00, 00), y: 14919.566});
+  arr.push({x: new Date(2013, 11 -1, 09, 20, 45, 00, 00), y: 14923.107});
+  arr.push({x: new Date(2013, 11 -1, 09, 21, 00, 00, 00), y: 14782.877});
+  arr.push({x: new Date(2013, 11 -1, 09, 21, 15, 00, 00), y: 14719.747});
+  arr.push({x: new Date(2013, 11 -1, 09, 21, 30, 00, 00), y: 14709.362});
+  arr.push({x: new Date(2013, 11 -1, 09, 21, 45, 00, 00), y: 14747.927});
+  arr.push({x: new Date(2013, 11 -1, 09, 22, 00, 00, 00), y: 14573.228});
+  arr.push({x: new Date(2013, 11 -1, 09, 22, 15, 00, 00), y: 14442.517});
+  arr.push({x: new Date(2013, 11 -1, 09, 22, 30, 00, 00), y: 14424.157});
+  arr.push({x: new Date(2013, 11 -1, 09, 22, 45, 00, 00), y: 14389.285});
+  arr.push({x: new Date(2013, 11 -1, 09, 23, 00, 00, 00), y: 14408.623});
+  arr.push({x: new Date(2013, 11 -1, 09, 23, 15, 00, 00), y: 14458.237});
+  arr.push({x: new Date(2013, 11 -1, 09, 23, 30, 00, 00), y: 14401.343});
+  arr.push({x: new Date(2013, 11 -1, 09, 23, 45, 00, 00), y: 14390.64});
 
   return [{
       values: arr,
